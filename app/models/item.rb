ae92e6a592646c938e7903b31faff71943b8859a
grzegorz9 <grzegorz.muszynski9@gmail.com>
2012-12-13 20:53:11 +0000

    Categories as acolumn in the items db table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ActiveRecord::Base
-  attr_accessible :description, :title, :category_id
+  attr_accessible :description, :title, :category_id, :subcategory
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
+  validates :subcategory, presence: true, length: { maximum: 20 }
   has_many :comments  
   belongs_to :category
 end
